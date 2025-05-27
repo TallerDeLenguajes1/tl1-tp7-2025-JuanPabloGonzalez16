c8b151ee60f315ada263575ba12b0d3e92b84dd8
Juan Pablo <juanpablofacultad16@hotmail.com>
2025-05-27 14:31:11 -0300

    Tercero

--- obj/Debug/net8.0/tl1-tp7-2025-JuanPabloGonzalez16.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl1-tp7-2025-JuanPabloGonzalez16.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl1-tp7-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e7b4219f5904f28d1113d00b4bbf8a0bbbadcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl1-tp7-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl1-tp7-2025-JuanPabloGonzalez16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
